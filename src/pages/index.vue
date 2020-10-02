925c40ec76e5307fb1f5b2f3a435e918014a0924
n2ptune <n2ptune@outlook.com>
2020-10-03 02:11:13 +0900

    deps: deleted logging fullpage instance in class constructor

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <client-only>
     <div class="fullpage-container">
+      <button class="next" @click="moveNext">Move Next</button>
       <div ref="example" v-fullpage="opts" class="fullpage-wp">
         <div class="page-1 page">
           <p class="part-1">fullpage-vue</p>
@@ -14,7 +15,6 @@
           <p class="part-3">fullpage-vue</p>
         </div>
       </div>
-      <button @click="moveNext">next</button>
     </div>
   </client-only>
 </template>
@@ -28,7 +28,12 @@ export default Vue.extend({
       opts: {
         start: 0,
         dir: 'v',
-        duration: 500
+        duration: 5500,
+        der: 1,
+        // @ts-ignore
+        beforeChange: (prev, next) => {
+          console.log(prev, next)
+        }
       }
     }
   },
@@ -42,6 +47,26 @@ export default Vue.extend({
 </script>
 
 <style lang="postcss" scoped>
+.fullpage-container {
+  & .next {
+    bottom: 15px;
+    right: 15px;
+
+    @apply fixed z-10 bg-white block;
+  }
+}
+
 .fullpage-wp {
+  & .page-1 {
+    @apply bg-red-300;
+  }
+
+  & .page-2 {
+    @apply bg-purple-300;
+  }
+
+  & .page-3 {
+    @apply bg-gray-300;
+  }
 }
 </style>
