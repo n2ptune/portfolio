9a1fef34e54e9248c616cdb68af0c662b79f71e1
n2ptune <n2ptune@outlook.com>
2021-02-19 18:29:36 +0900

    feat: mixins, changed style

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <main class="fonts-raleway">
-    <section class="top">
-      <h2>MAKE</h2>
-      <h2>CREATIVE</h2>
-      <h2>DEVELOPER</h2>
-    </section>
-    <section class="bottom">
-      <h3>SITE REBUILDING</h3>
-      <h3>COMMING SOON</h3>
-    </section>
+    <article class="top">
+      <section>
+        <h2>MAKE</h2>
+        <h2>CREATIVE</h2>
+        <h2>DEVELOPER</h2>
+      </section>
+    </article>
+    <article class="bottom">
+      <section>
+        <h3>SITE REBUILDING</h3>
+        <h3>COMMING SOON</h3>
+      </section>
+    </article>
   </main>
 </template>
 
@@ -17,33 +21,50 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
+@import '@/styles/mixins.scss';
+
 main {
   display: flex;
   align-items: center;
-  flex-direction: column;
+  justify-content: center;
+  flex-wrap: wrap;
   height: 100%;
   position: relative;
 
+  > article {
+    width: 100%;
+  }
+
   .top {
     position: relative;
-    top: 10rem;
     text-align: center;
 
+    > section {
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+    }
+
     h2 {
+      display: inline-block;
+      white-space: pre-line;
       margin: 0;
       padding: 0;
       line-height: 1.3;
       letter-spacing: 2px;
 
       &:nth-of-type(2) {
+        position: relative;
+
         &::before {
           content: '';
-          display: flex;
+          display: block;
+          top: 0;
+          left: -25px;
           position: absolute;
-          width: 100%;
+          width: calc(100% + 50px);
           height: 100%;
-          padding: 5px 3px;
-          background-color: #760c96;
+          background-color: #9037ac;
           z-index: -2;
         }
 
@@ -54,16 +75,18 @@ main {
   }
 
   .bottom {
-    margin-bottom: 20rem;
     position: relative;
-    top: 35rem;
     text-align: center;
     color: rgba(0, 0, 0, 0.35);
 
     h3 {
       margin: 0;
       line-height: 1.7;
-      letter-spacing: 25px;
+      letter-spacing: 10px;
+
+      @include desktop {
+        letter-spacing: 20px;
+      }
     }
   }
 }
